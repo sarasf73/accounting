7f2dc53d9c3fc1ecac763b5cacfff336e0158004
sara <saraebi221@gmail.com>
2021-08-26 13:16:14 +0430

    create accounting tables

---
+++ database/migrations/2021_08_25_075832_create_documents_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDocumentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('documents', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('type_id')->constrained('document_types','id');
+            $table->tinyInteger('status');
+            $table->text('description');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('documents');
+    }
+}
---
+++ database/migrations/2021_08_26_080331_create_docitems_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDocitemsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('docitems', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('debit');
+            $table->unsignedBigInteger('credit');
+            $table->foreignId('document_id')->constrained('documents','id');
+            $table->foreignId('book_id')->constrained('books','id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('docitems');
+    }
+}
